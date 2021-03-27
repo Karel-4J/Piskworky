15e3ae7b34181b7eb11600847f6843df1ead8596
Karel Ilavsky <karel.ilavsky@4jtech.cz>
2021-03-27 16:21:16 +0100

    druhy commit

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 # coding=utf-8
+winner = 0
 
-print("Kurva nic nemam :-/")
+history = []
+cord_x = []
+cord_y = []
+
+print ("Make a move! (Write coordinates x,y)")
+
+while winner == 0:
+    makeMove = input()
+    history.append(makeMove)
+
+    if len(makeMove) == 2:
+        cord_x.append(makeMove[0])
+        cord_y.append(makeMove[1])
+    else:
+        print("Write coordinates x,y (example: 0,0)")
+    print (cord_y)
+    # Vytvořit pole
+
+    # Checknout ho
+
+
+
+
+
