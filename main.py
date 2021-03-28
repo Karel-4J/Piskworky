922b7c45e9978f7858786a602c9d743ff0e6372b
Karl <karel.ilavsky@4jtech.cz>
2021-03-28 17:21:47 +0200

    Finale... ?

--- main.py
+++ main.py
@@ -1,27 +1,131 @@
 # coding=utf-8
-winner = 0
+# input fungus jen v 2.7
 
-history = []
-cord_x = []
-cord_y = []
+def check_winner(moves_like_jagger):
 
-print ("Make a move! (Write coordinates x,y)")
+    for n in moves_like_jagger:
+        count = 1
+        for x_check in range(1, 6, 1):
+            temp = (n[0] + x_check, n[1])
+            if temp in moves_like_jagger:
+                count += 1
+            else:
+                break
+        if count == 5:
+            return True
+        count = 1
+        for diagonal_plus_check in range(1, 6, 1):
+            temp = (n[0] + diagonal_plus_check, n[1] + diagonal_plus_check)
+            if temp in moves_like_jagger:
+                count += 1
+            else:
+                break
+        if count == 5:
+            return True
+        count = 1
+        for y_check in range(1, 6, 1):
+            temp = (n[0], n[1] + y_check)
+            if temp in moves_like_jagger:
+                count += 1
+            else:
+                break
+        if count == 5:
+            return True
+        count = 1
+        for diagonal_minus_check in range(1, 6, 1):
+            temp = (n[0] + diagonal_minus_check, n[1] - diagonal_minus_check)
+            if temp in moves_like_jagger:
+                count += 1
+            else:
+                break
+        if count == 5:
+            return True
 
-while winner == 0:
+
+winner = False
+message = ""
+turn = 0
+max_y = 0
+min_y = 0
+max_x = 0
+min_x = 0
+podvody = False
+
+# dick_pole = {
+#                 "player_1": [(-1, 1), (-2, 2), (-3, 3), (-4, 4)],
+#                 "player_2": []
+#             }
+
+dick_pole = {
+    "player_1": [],
+    "player_2": []
+}
+
+print("Make a move! (Write coordinates x,y)")
+
+while not winner:
+
     makeMove = input()
-    history.append(makeMove)
-
     if len(makeMove) == 2:
-        cord_x.append(makeMove[0])
-        cord_y.append(makeMove[1])
+
+        for i in dick_pole["player_2"]:
+            if makeMove == i:
+                print("Ty podvodníku!")
+                podvody = True
+                break
+        for i in dick_pole["player_1"]:
+            if makeMove == i:
+                print("Ty podvodníku!")
+                podvody = True
+                break
+        if podvody:
+            podvody = False
+            continue
+        if makeMove[0] >= max_x - 3:
+            max_x = makeMove[0] + 3
+        if makeMove[0] <= min_x + 2:
+            min_x = makeMove[0] - 2
+        if makeMove[1] >= max_y - 3:
+            max_y = makeMove[1] + 3
+        if makeMove[1] <= min_y + 2:
+            min_y = makeMove[1] - 2
+
+        turn += 1
+        print("turn: " + str(turn))
+
+        if turn % 2 == 1:
+            dick_pole["player_1"].append(makeMove)
+            winner = check_winner(dick_pole["player_1"])
+            message = "1"
+        else:
+            dick_pole["player_2"].append(makeMove)
+            winner = check_winner(dick_pole["player_2"])
+            message = "2"
     else:
         print("Write coordinates x,y (example: 0,0)")
-    print (cord_y)
-    # Vytvořit pole
 
-    # Checknout ho
+    pole = ""
+    for ii in range(min_y, max_y, 1):
+        if ii < 0:
+            col = str(ii) + "|"
+        else:
+            col = " " + str(ii) + "|"
 
-
-
-
-
+        for i in range(min_x, max_x, 1):
+            if ii == min_y:
+                if i < 0:
+                    col = col + str(i) + " |"
+                else:
+                    col = col + " " + str(i) + " |"
+            else:
+                if (i, ii) in dick_pole["player_1"]:
+                    col = col + " x |"
+                elif (i, ii) in dick_pole["player_2"]:
+                    col = col + " o |"
+                else:
+                    col = col + "   |"
+        lang = max_x-min_x+1
+        pole = pole + col + "\n " + lang * "-+--" + "\n"
+    print(pole)
+else:
+    print("Winner is player " + message + "!")
